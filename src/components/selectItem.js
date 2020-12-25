3b638b9eb882597631cf414935ed50d52a63abe8
Nishal Kulkarni <nishalkulkarni@gmail.com>
2020-12-25 18:40:39 +0530

    made responsive

--- src/components/selectItem.js
+++ src/components/selectItem.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 const SelectItem = (props) => (
-  <div key={props.id} className={`flex divide-solid p-6`}>
-    <div className={`flex-1 text-left`}>
+  <div key={props.id} className={`flex flex-col sm:flex-row divide-solid p-6`}>
+    <div className={`flex-none sm:text-left sm:flex-1`}>
       <h4>{props.name}</h4>
     </div>
-    <div className={`flex-initial`}>
+    <div className={`flex-none sm:flex-initial`}>
       <h4>{props.regno}</h4>
     </div>
-    <div className={`flex-1 text-right`}>
+    <div className={`flex-none sm:text-right sm:flex-1`}>
       {props.domain === "TECHNICAL" && (
         <button
           className={`uppercase text-base font-semibold px-4 rounded bg-green-300 text-green-600 max-w-max shadow-sm`}
